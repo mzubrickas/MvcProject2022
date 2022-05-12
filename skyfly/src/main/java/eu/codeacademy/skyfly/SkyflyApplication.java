1fdfa4deb4b50f5b1ef21a1627204e400a795d8d
Mantas <mantaszubrickas@inbox.lt>
2022-05-12 19:48:01 +0300

    init skyfly project

---
+++ skyfly/src/main/java/eu/codeacademy/skyfly/SkyflyApplication.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.skyfly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyflyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyflyApplication.class, args);
+    }
+
+}
