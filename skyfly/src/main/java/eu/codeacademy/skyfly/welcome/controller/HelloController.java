65e9e9d9a0a4bc8e3213a81f94f67489b067f15a
Mantas <mantaszubrickas@inbox.lt>
2022-05-22 22:04:42 +0300

    1 - init new sky-project branch, created welcome page with form (place, date)

---
+++ skyfly/src/main/java/eu/codeacademy/skyfly/welcome/controller/HelloController.java
@@ -0,0 +1,13 @@
+package eu.codeacademy.skyfly.welcome.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "welcome/hello";
+    }
+}
