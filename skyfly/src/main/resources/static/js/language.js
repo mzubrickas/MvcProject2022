dd24c10c4640431d01108cac5a6969a9d85eac7c
Mantas <mantaszubrickas@inbox.lt>
2022-05-31 23:05:07 +0300

    8 -- implemented language logic (translated home.html, nav bar, and some places)

---
+++ skyfly/src/main/resources/static/js/language.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $("#locales").change(function () {
+        let selectedOption = $('#locales').val();
+        if (selectedOption !== ''){
+            window.location.replace('?lang=' + selectedOption);
+        }
+    });
+});
